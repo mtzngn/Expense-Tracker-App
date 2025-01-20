0e92df8db63d3e6f641e763950bf16f7d107c176
Taha <mt.zngn@gmail.com>
2025-01-20 17:36:17 +0000

    added navigations for login and sign up

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -6,14 +6,19 @@ import { colors, spacingX, spacingY } from "@/constants/theme";
 import { verticalScale } from "@/utils/styling";
 import Button from "@/components/Button";
 import Animated, { FadeIn, FadeInDown } from "react-native-reanimated";
+import { useRouter } from "expo-router";
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <ScreenWrapper>
       <View style={styles.container}>
         {/* login button & image */}
         <View>
-          <TouchableOpacity style={styles.loginButton}>
+          <TouchableOpacity
+            onPress={() => router.push("/(auth)/login")}
+            style={styles.loginButton}
+          >
             <Typo fontWeight={"500"}> Sign in</Typo>
           </TouchableOpacity>
 
@@ -60,7 +65,7 @@ const Welcome = () => {
               .springify()
               .damping(12)}
           >
-            <Button>
+            <Button onPress={() => router.push("/(auth)/register")}>
               <Typo size={22} color={colors.neutral900} fontWeight={600}>
                 Get Started
               </Typo>
