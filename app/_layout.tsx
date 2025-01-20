61a0ae97c168122a479550b6d8f1a3d9e6743529
Taha <mt.zngn@gmail.com>
2025-01-20 12:57:08 +0000

    index and layout pages updated. index page routes the user to welcome page after 2 seconds.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout() {
-  return <Stack />;
-}
+const _layout = () => {
+  return <Stack screenOptions={{ headerShown: false }} />;
+};
+
+export default _layout;
