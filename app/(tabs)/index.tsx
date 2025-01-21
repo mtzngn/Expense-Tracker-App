be3f08636e5b2f2506984403f40ff3f670a97fd9
Taha <mt.zngn@gmail.com>
2025-01-21 07:58:24 +0000

    basic logic to logout user, its working.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { colors } from "@/constants/theme";
+import Button from "@/components/Button";
+import Typo from "@/components/Typo";
+import { signOut } from "firebase/auth";
+import { auth } from "@/config/firebase";
 
 const Home = () => {
+  const handleLogout = async () => {
+    await signOut(auth);
+  };
   return (
     <View>
       <Text>Home</Text>
+      <Button onPress={handleLogout}>
+        <Typo color={colors.black}>Logout</Typo>
+      </Button>
     </View>
   );
 };
