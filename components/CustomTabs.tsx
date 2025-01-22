f989cf1910b3898472b401ad175db6d583295120
Taha <mt.zngn@gmail.com>
2025-01-22 11:19:31 +0000

    created a custom tab component

---
+++ components/CustomTabs.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const CustomTabs = () => {
+  return (
+    <View>
+      <Text>CustomTabs</Text>
+    </View>
+  );
+};
+
+export default CustomTabs;
+
+const styles = StyleSheet.create({});
