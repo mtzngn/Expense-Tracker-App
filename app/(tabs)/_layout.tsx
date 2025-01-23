ae1b907d2c6ce82e1c0f74c169d2b97a4af92ca1
Taha <mt.zngn@gmail.com>
2025-01-23 18:48:32 +0000

    hide header

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,7 +5,7 @@ import CustomTabs from "@/components/CustomTabs";
 
 const _layout = () => {
   return (
-    <Tabs tabBar={CustomTabs} screenOptions={{ headerShown: true }}>
+    <Tabs tabBar={CustomTabs} screenOptions={{ headerShown: false }}>
       <Tabs.Screen name="index" />
       <Tabs.Screen name="statistics" />
       <Tabs.Screen name="wallet" />
