aae02e40d676e6b84ca74a7fd508a69fec193133
Taha <mt.zngn@gmail.com>
2025-01-24 22:57:33 +0000

    profile screen styling added

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
+import { verticalScale } from "@/utils/styling";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
 
 const Profile = () => {
   return (
@@ -12,4 +14,64 @@ const Profile = () => {
 
 export default Profile;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: verticalScale(30),
+  },
+  userInfo: {
+    marginTop: verticalScale(30),
+    alignItems: "center",
+    gap: spacingY._15,
+  },
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignSelf: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+    // overflow: 'hidden',
+    // position: 'relative',
+  },
+  editIcon: {
+    position: "absolute",
+    bottom: 5,
+    right: 8,
+    borderRadius: 50,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: 5,
+  },
+  nameContainer: {
+    gap: verticalScale(4),
+    alignItems: "center",
+  },
+  lastIcon: {
+    height: verticalScale(44),
+    width: verticalScale(44),
+    backgroundColor: colors.neutral500,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: radius._15,
+    borderCurve: "continuous",
+  },
+  lastItem: {
+    marginBottom: verticalScale(17),
+  },
+  accountOption: {
+    marginTop: spacingY._35,
+  },
+  flexRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: spacingX._10,
+  },
+});
