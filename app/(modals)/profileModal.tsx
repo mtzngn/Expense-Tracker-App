a2ce8edb3da3d4628519f14fde9697a9afa3dca3
Taha <mt.zngn@gmail.com>
2025-01-25 11:51:31 +0000

    Created a profile modal

---
+++ app/(modals)/profileModal.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const profileModal = () => {
+  return (
+    <View>
+      <Text>profileModal</Text>
+    </View>
+  );
+};
+
+export default profileModal;
+
+const styles = StyleSheet.create({});
