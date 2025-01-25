e70d60f144c72c9834bfad2b91d8cde023957e5b
Taha <mt.zngn@gmail.com>
2025-01-25 11:47:55 +0000

    no longer used

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,15 +8,15 @@ import { auth } from "@/config/firebase";
 import ScreenWrapper from "@/components/ScreenWrapper";
 
 const Home = () => {
-  const handleLogout = async () => {
-    await signOut(auth);
-  };
+  // const handleLogout = async () => {
+  //   await signOut(auth);
+  // };
   return (
     <ScreenWrapper>
       <Typo>Home</Typo>
-      <Button onPress={handleLogout}>
+      {/* <Button onPress={handleLogout}>
         <Typo color={colors.black}>Logout</Typo>
-      </Button>
+      </Button> */}
     </ScreenWrapper>
   );
 };
