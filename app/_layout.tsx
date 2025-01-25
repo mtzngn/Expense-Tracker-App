8dadc636deb060883dcc684160b93432373ad528
Taha <mt.zngn@gmail.com>
2025-01-25 11:51:49 +0000

    add profile modal to navigation stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,14 @@ import { AuthProvider } from "@/contexts/authContext";
 import { Stack } from "expo-router";
 
 const StackLayout = () => {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="(modals)/profileModal"
+        options={{ presentation: "modal" }}
+      />
+    </Stack>
+  );
 };
 
 export default function RootLayout() {
