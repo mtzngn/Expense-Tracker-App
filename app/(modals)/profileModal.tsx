db6e74192946b63db2df61c2845795cd25ea1410
Taha <mt.zngn@gmail.com>
2025-01-26 14:59:50 +0000

    created a profile modal where user data can be updated

--- app/(modals)/profileModal.tsx
+++ app/(modals)/profileModal.tsx
@@ -1,14 +1,178 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Alert,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
+import ModalWrapper from "@/components/ModalWrapper";
+import Header from "@/components/Header";
+import BackButton from "@/components/BackButton";
+import { Image } from "expo-image";
+import { getProfileImage } from "@/services/imageService";
+import * as Icons from "phosphor-react-native";
+import Typo from "@/components/Typo";
+import Input from "@/components/Input";
+import { UserDataType } from "@/types";
+import Button from "@/components/Button";
+import { useAuth } from "@/contexts/authContext";
+import { updateUser } from "@/services/userService";
+import { useRouter } from "expo-router";
+import * as ImagePicker from "expo-image-picker";
 
 const profileModal = () => {
+  const { user, updateUserData } = useAuth();
+  const [userData, setUserData] = useState<UserDataType>({
+    name: "",
+    image: null,
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    setUserData({
+      name: user?.name || "",
+      image: user?.image || null,
+    });
+  }, [user]);
+
+  const onSubmit = async () => {
+    let { name, image } = userData;
+    if (!name.trim()) {
+      Alert.alert("User", "Please fill all the fields.");
+      return;
+    }
+    setIsLoading(true);
+    const res = await updateUser(user?.uid as string, userData);
+    setIsLoading(false);
+
+    if (res.success) {
+      updateUserData(user?.uid as string);
+      router.back();
+    } else {
+      Alert.alert("", res.msg);
+    }
+  };
+
+  const onPickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      //   allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.5,
+    });
+    console.log(result);
+    console.log("result.assets[0]", result.assets[0]);
+
+    if (!result.canceled) {
+      setUserData({ ...userData, image: result.assets[0] });
+    }
+  };
   return (
-    <View>
-      <Text>profileModal</Text>
-    </View>
+    <ModalWrapper>
+      <View style={styles.container}>
+        <Header
+          title="Update Profile"
+          leftIcon={<BackButton />}
+          style={{ marginBottom: spacingY._10 }}
+        />
+        {/* form */}
+        <ScrollView contentContainerStyle={styles.form}>
+          <View style={styles.avatarContainer}>
+            <Image
+              style={styles.avatar}
+              source={getProfileImage(userData.image)}
+              contentFit="cover"
+              transition={100}
+            />
+
+            <TouchableOpacity style={styles.editIcon} onPress={onPickImage}>
+              <Icons.Pencil
+                size={verticalScale(20)}
+                color={colors.neutral800}
+              />
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.inputContainer}>
+            <Typo color={colors.neutral200}>Name</Typo>
+            <Input
+              placeholder="Name"
+              value={userData.name}
+              onChangeText={(value) => {
+                setUserData({ ...userData, name: value });
+              }}
+            />
+          </View>
+        </ScrollView>
+      </View>
+
+      <View style={styles.footer}>
+        <Button onPress={onSubmit} style={{ flex: 1 }} loading={isLoading}>
+          <Typo color={colors.black} fontWeight={"700"}>
+            Update
+          </Typo>
+        </Button>
+      </View>
+    </ModalWrapper>
   );
 };
 
 export default profileModal;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingHorizontal: spacingY._20,
+  },
+  footer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: spacingX._20,
+    gap: verticalScale(12),
+    paddingTop: spacingY._15,
+    borderTopColor: colors.neutral700,
+    marginBottom: spacingY._5,
+    borderTopWidth: 1,
+  },
+  form: {
+    gap: spacingY._30,
+    marginTop: spacingY._15,
+  },
+
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignSelf: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+    borderWidth: 1,
+    borderColor: colors.neutral500,
+  },
+  editIcon: {
+    position: "absolute",
+    bottom: spacingY._5,
+    right: spacingY._7,
+    borderRadius: 100,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: spacingY._7,
+  },
+  inputContainer: {
+    gap: spacingY._10,
+  },
+});
