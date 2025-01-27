cfcb543b980699f9a499d1d6fa2107f1f3285c8c
Taha <mt.zngn@gmail.com>
2025-01-27 17:33:06 +0000

    image service updated to handle saving an avatar image

--- services/imageService.ts
+++ services/imageService.ts
@@ -1,3 +1,45 @@
+import { CLOUDINAR_CLOUD_NAME, CLOUDINAY_UPLOAD_PRESET } from "@/constants";
+import { ResponseType } from "@/types";
+import axios from "axios";
+
+const CLOUDINAYR_CLOUD_URL = `https://api.cloudinary.com/v1_1/${CLOUDINAR_CLOUD_NAME}/image/upload`;
+
+export const uploadFileToCloudinary = async (
+  file: { uri?: string } | string,
+  folderName: string
+): Promise<ResponseType> => {
+  try {
+    if (typeof file == "string") {
+      return { success: true, data: file };
+    }
+    if (file && file.uri) {
+      const formData = new FormData();
+      formData.append("file", {
+        uri: file?.uri,
+        type: "image/jpeg",
+        name: file?.uri?.split("/").pop() || "file.jpg",
+      } as any);
+
+      formData.append("upload_preset", CLOUDINAY_UPLOAD_PRESET);
+      formData.append("folder", folderName);
+
+      const response = await axios.post(CLOUDINAYR_CLOUD_URL, formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      return { success: true, data: response?.data?.secure_url };
+    }
+  } catch (error: any) {
+    console.log(
+      "error uploading the file:",
+      error || "Could not upload the file"
+    );
+    return { success: false, msg: error.message };
+  }
+};
+
 export const getProfileImage = (file: any) => {
   if (file && typeof file == "string") return file;
   if (file && typeof file == "object") return file.uri;
