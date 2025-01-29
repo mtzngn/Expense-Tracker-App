19f5f47ba1af0e047e0bce7ab4f1a28e8e304469
Taha <mt.zngn@gmail.com>
2025-01-29 10:51:20 +0000

    prepare wallet screen with styling

--- app/(tabs)/wallet.tsx
+++ app/(tabs)/wallet.tsx
@@ -1,15 +1,43 @@
-import { StyleSheet, Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import ScreenWrapper from "@/components/ScreenWrapper";
+import { colors, radius, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
 
 const Wallet = () => {
   return (
-    <ScreenWrapper>
-      <Text>Wallet</Text>
+    <ScreenWrapper style={{ backgroundColor: colors.black }}>
+      <View style={styles.container}></View>
     </ScreenWrapper>
   );
 };
 
 export default Wallet;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "space-between" },
+  balanceView: {
+    height: verticalScale(160),
+    backgroundColor: colors.black,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  flexRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: spacingY._10,
+  },
+  wallets: {
+    flex: 1,
+    backgroundColor: colors.neutral900,
+    borderTopEndRadius: radius._30,
+    borderTopLeftRadius: radius._30,
+    padding: spacingX._20,
+    paddingTop: spacingX._25,
+  },
+  listStyle: {
+    paddingVertical: spacingY._25,
+    paddingTop: spacingY._15,
+  },
+});
