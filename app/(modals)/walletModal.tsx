442414ff81a92ec5703fcff730d3c9db8aa194e4
Taha <mt.zngn@gmail.com>
2025-02-03 11:01:27 +0000

    implemented a wallet modal where new wallets can be created

--- app/(modals)/walletModal.tsx
+++ app/(modals)/walletModal.tsx
@@ -1,16 +1,146 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Alert, ScrollView, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import { colors, spacingX, spacingY } from "@/constants/theme";
+import { verticalScale } from "@/utils/styling";
 import ModalWrapper from "@/components/ModalWrapper";
+import Header from "@/components/Header";
+import BackButton from "@/components/BackButton";
 import Typo from "@/components/Typo";
+import Input from "@/components/Input";
+import { WalletType } from "@/types";
+import Button from "@/components/Button";
+import { useAuth } from "@/contexts/authContext";
+import { useRouter } from "expo-router";
+import ImageUpload from "@/components/ImageUpload";
+import { createOrUpdateWallet } from "@/services/walletService";
 
 const WalletModal = () => {
+  const { user } = useAuth();
+  const [wallet, setWallet] = useState<WalletType>({
+    name: "",
+    image: null,
+  });
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  const onSubmit = async () => {
+    let { name, image } = wallet;
+    if (!name.trim() || !image) {
+      Alert.alert("Wallet", "Please fill all the fields.");
+      return;
+    }
+    const data: WalletType = {
+      name,
+      image,
+      uid: user?.uid,
+    };
+    // todo: include wallet id if updating
+    setIsLoading(true);
+    const res = await createOrUpdateWallet(data);
+    setIsLoading(false);
+
+    if (res.success) {
+      router.back();
+    } else {
+      Alert.alert("Wallet", res.msg);
+    }
+  };
+
   return (
     <ModalWrapper>
-      <Typo>WalletModal</Typo>
+      <View style={styles.container}>
+        <Header
+          title="New Wallet"
+          leftIcon={<BackButton />}
+          style={{ marginBottom: spacingY._10 }}
+        />
+        {/* form */}
+        <ScrollView contentContainerStyle={styles.form}>
+          <View style={styles.inputContainer}>
+            <Typo color={colors.neutral200}>Wallet Name</Typo>
+            <Input
+              placeholder="Salary"
+              value={wallet.name}
+              onChangeText={(value) => {
+                setWallet({ ...wallet, name: value });
+              }}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Typo color={colors.neutral200}>Wallet Icon</Typo>
+            {/* image input */}
+            <ImageUpload
+              file={wallet.image}
+              onSelect={(file) => setWallet({ ...wallet, image: file })}
+              onClear={() => setWallet({ ...wallet, image: null })}
+              placeholder="Upload Image"
+            />
+          </View>
+        </ScrollView>
+      </View>
+
+      <View style={styles.footer}>
+        <Button onPress={onSubmit} style={{ flex: 1 }} loading={isLoading}>
+          <Typo color={colors.black} fontWeight={"700"}>
+            Add Wallet
+          </Typo>
+        </Button>
+      </View>
     </ModalWrapper>
   );
 };
 
 export default WalletModal;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-between",
+    paddingHorizontal: spacingY._20,
+  },
+  footer: {
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "center",
+    paddingHorizontal: spacingX._20,
+    gap: verticalScale(12),
+    paddingTop: spacingY._15,
+    borderTopColor: colors.neutral700,
+    marginBottom: spacingY._5,
+    borderTopWidth: 1,
+  },
+  form: {
+    gap: spacingY._30,
+    marginTop: spacingY._15,
+  },
+
+  avatarContainer: {
+    position: "relative",
+    alignSelf: "center",
+  },
+  avatar: {
+    alignSelf: "center",
+    backgroundColor: colors.neutral300,
+    height: verticalScale(135),
+    width: verticalScale(135),
+    borderRadius: 200,
+    borderWidth: 1,
+    borderColor: colors.neutral500,
+  },
+  editIcon: {
+    position: "absolute",
+    bottom: spacingY._5,
+    right: spacingY._7,
+    borderRadius: 100,
+    backgroundColor: colors.neutral100,
+    shadowColor: colors.black,
+    shadowOffset: { width: 0, height: 0 },
+    shadowOpacity: 0.25,
+    shadowRadius: 10,
+    elevation: 4,
+    padding: spacingY._7,
+  },
+  inputContainer: {
+    gap: spacingY._10,
+  },
+});
