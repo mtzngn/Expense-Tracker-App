1a20f3ccb6c7660b722642f0e179e8614f6c042c
Taha <mt.zngn@gmail.com>
2025-02-05 00:05:51 +0000

    home card created to be used in home tab

---
+++ components/HomeCard.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Typo from "./Typo";
+
+const HomeCard = () => {
+  return (
+    <View>
+      <Typo>HomeCard</Typo>
+    </View>
+  );
+};
+
+export default HomeCard;
+
+const styles = StyleSheet.create({});
